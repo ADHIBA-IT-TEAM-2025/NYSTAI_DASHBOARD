29267c254c1686bc150df42bd98d3b20411c77e3
ADHIBA-IT-TEAM-2025 <sanjeev@adhiba.com>
2025-08-13 09:49:19 +0530

    vercel deploy 01

--- vite.config.ts
+++ vite.config.ts
@@ -2,26 +2,17 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     svgr({
       svgrOptions: {
         icon: true,
+        // This will transform your SVG to a React component
         exportType: "named",
         namedExport: "ReactComponent",
       },
     }),
   ],
-  base: "/", // <-- Important: base path for your app
-  build: {
-    outDir: "dist",
-  },
-  server: {
-    port: 5173,
-    open: true,
-  },
-  preview: {
-    port: 5173,
-  },
 });
